74361e997bb36100a3fca86bedcdac7c5d0e0bcc
YuvaPrasath <syuvaprasath7@gmail.com>
2023-02-19 14:28:17 +0530

    index commit

--- Task/index.js
+++ Task/index.js
@@ -1,39 +1,40 @@
 
-function Product(name, price) {
-  this.Name = name;
-  this.Price = price;
-}
-function Food(Name, Price) {
-  Product.call(this, Name, Price);
-}
-console.log(new Food("box", 5).Name);
+// function Product(name, price) {
+//   this.Name = name;
+//   this.Price = price;
+// }
+// function Food(Name, Price) {
+//   Product.call(this, Name, Price);
+// }
+// console.log(new Food("box", 5).Name);
 
-A = "yuvaprasath";
-B = A[2];
-console.log(B);
+// A = "yuvaprasath";
+// B = A[2];
+// console.log(B);
 
-function Name(x, y) {
-  return x + y;
-}
-console.log(Name(2, 3));
+// function Name(x, y) {
+//   return x + y;
+// }
+// console.log(Name(2, 3));
 
 
-var arry = [5, 7, 6, 33, 45, 9, 150, 80, 70];
-debugger;
-function rearrangeArray(arry) {
-  const result = [];
-  arry.sort((a, b) => a-b);
-  let i = 0,
-    j = arry.length - 1;
-  while (i <= j) {
-    result.push(arry[i]);
-    i++;
-    if (i <= j) {
-      result.push(arry[j]);
-      j--;
-    }
-  }
-  return result;
-}
-const result = rearrangeArray(arry);
-console.log(result)
+// var arry = [5, 7, 6, 33, 45, 9, 150, 80, 70];
+// debugger;
+// function rearrangeArray(arry) {
+//   const result = [];
+//   arry.sort((a, b) => a-b);
+//   let i = 0,
+//     j = arry.length - 1;
+//   while (i <= j) {
+//     result.push(arry[i]);
+//     i++;
+//     if (i <= j) {
+//       result.push(arry[j]);
+//       j--;
+//     }
+//   }
+//   return result;
+// }
+// const result = rearrangeArray(arry);
+// console.log(result)
+
